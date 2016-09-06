3729b5fc19cc064a1b48c33a5e125384366ccfd9
Steve OC <stephen@ocallaghan.me.uk>
2016-09-06 21:10:25 +0100

    Init

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthenication: function(req, res, next){
+		console.log('private page hit!!!');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get ('/about', middleware.requireAuthenication, function (req, res){
+	res.send('About Me');
+});
+
+app.use(express.static (__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('Express server has started on port ' + PORT);	
+});
