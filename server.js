ae12324bb5b28197e30afee5bef711abff831967
Steve OC <stephen@ocallaghan.me.uk>
2016-09-06 21:19:44 +0100

    middleware

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var middleware = require('./middleware.js');
 
-var middleware = {
-	requireAuthenication: function(req, res, next){
-		console.log('private page hit!!!');
-		next();
-	},
-	logger: function(req, res, next){
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
 
 app.use(middleware.logger);
 
